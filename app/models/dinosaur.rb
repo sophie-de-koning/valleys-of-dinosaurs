95cda042604a7d83f92d5873fa15b3f634318b95
sophie-de-koning <sophiadekoning@gmail.com>
2018-01-12 17:21:58 +0100

    Add dinosaur validations

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,7 +1,9 @@
 class Dinosaur < ApplicationRecord
 
-  def is_baby?
-    age < 3
-  end
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 5 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
 
 end
